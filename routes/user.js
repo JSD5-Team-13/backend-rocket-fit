a73c23ab4b6804981d2e25659f0a0f43c582991c
Falaloon <sr.ratchatapusit@gmail.com>
2023-10-22 18:51:00 +0700

    Update users path

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/users');
-const auth = require('../middleware/auth');
+const Users = require("../models/users");
+const auth = require("../middleware/auth");
 
-router.get('/', auth, async (req, res) => {
-    try {
-        const user = req.user;
-        res.status(200).json(user);
-    } catch (error) {
-        console.error('Error fetching user data:', error);
-        res.status(500).json({ error: 'Error fetching user data' });
+router.get("/", auth, async (req, res) => {
+  try {
+    const { username } = req.user;
+    const userData = await Users.findOne({ username });
+
+    if (!userData) {
+      res.status(404).json({ error: "User not found" });
     }
+
+    const dob = new Date(userData.DateOfBirth);
+    const age = new Date().getFullYear() - dob.getFullYear();
+
+    res.status(200).json({
+      id: userData._id,
+      username: userData.username,
+      user_status: userData.user_status,
+      weight: userData.weight,
+      height: userData.height,
+      age,
+    });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    res.status(500).json({ error: "Error fetching user data" });
+  }
 });
 
 module.exports = router;
